81d7d7ebccb1678422f2345e2290623dcd8681ad
wahabshop <wahabmari751@gmail.com>
2024-09-23 22:23:28 +0600

    comment setup

--- js/popUp.js
+++ js/popUp.js
@@ -4,19 +4,19 @@ const closePopupButton = document.getElementById('closePopup');
 const overlay = document.getElementById('overlay');
 const popup = document.getElementById('popup');
 
-// Open the popup
+
 openPopupButton.addEventListener('click', function() {
   overlay.classList.remove('hidden');
   popup.classList.remove('hidden');
 });
 
-// Close the popup
+
 closePopupButton.addEventListener('click', function() {
   overlay.classList.add('hidden');
   popup.classList.add('hidden');
 });
 
-// Close the popup when clicking outside (on the overlay)
+
 overlay.addEventListener('click', function() {
   overlay.classList.add('hidden');
   popup.classList.add('hidden');
